796dc1a6a3abcf6919bb6af5f0a840f81713764f
Andie <zchangan@163.com>
2017-09-22 15:16:31 +0800

    clear all

--- Exception/src/main/java/cn/andiedie/exception/MyException.java
+++
@@ -1,7 +0,0 @@
-package cn.andiedie.exception;
-
-public class MyException extends Exception {
-    public MyException(String message) {
-        super(message);
-    }
-}
--- RESTful-Demo/src/main/java/cn/andiedie/exceptions/UserExistException.java
+++
@@ -1,7 +0,0 @@
-package cn.andiedie.exceptions;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(value = HttpStatus.CONFLICT, reason = "Already Exists")
-public class UserExistException extends RuntimeException { }
