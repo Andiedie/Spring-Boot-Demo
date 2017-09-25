b2c651809601d28ed4a7bb1afb523269b1c61174
Andie <zchangan@163.com>
2017-09-25 20:16:42 +0800

    5.swagger-api-doc

---
+++ 5.swagger-api-doc/src/main/java/cn/andiedie/swaggerapidoc/SwaggerApiDocApplication.java
@@ -0,0 +1,12 @@
+package cn.andiedie.swaggerapidoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggerApiDocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwaggerApiDocApplication.class, args);
+	}
+}
